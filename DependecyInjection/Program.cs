aeee7d6401d9b9e60a0084ce4cee783ad1e4f293
Yadav <ky185041@ncr.com>
2020-04-01 15:43:19 +0530

    Added Files

---
+++ DependecyInjection/Program.cs
@@ -0,0 +1,27 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependecyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+
+            container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
+            container.Register(Component.For<IConsoleWriter>().ImplementedBy<ConsoleWriter>());
+
+            var root = container.Resolve<ICompositionRoot>();
+
+
+            root.LogMessage("Hello from my very first resolved class`");
+            Console.ReadLine();
+        }
+    }
+}
